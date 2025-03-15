46035eaf5b533e81ef440fd8e487c48c732b79c5
Rafael Alexandre <rafaelxndre@gmail.com>
2025-03-15 19:16:42 -0300

    Backend project created

---
+++ backend/src/main/java/com/go2Win/agendamentotransf/AgendamentotransfApplication.java
@@ -0,0 +1,13 @@
+package com.go2Win.agendamentotransf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentotransfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentotransfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/go2Win/agendamentotransf/AgendamentotransfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go2Win.agendamentotransf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentotransfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
