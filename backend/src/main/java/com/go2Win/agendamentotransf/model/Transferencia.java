db564b96272c715c766aeea13e42a7d65636e90e
Rafael Alexandre <rafaelxndre@gmail.com>
2025-03-16 00:36:04 -0300

    Configuração de banco de dados H2 e entidades

---
+++ backend/src/main/java/com/go2Win/agendamentotransf/model/Transferencia.java
@@ -0,0 +1,75 @@
+package com.go2Win.agendamentotransf.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Transferencia {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String contaOrigem;
+	private String contaDestino;
+	private BigDecimal valor;
+	private BigDecimal taxa;
+	private LocalDate dataAgendamento;
+	private LocalDate dataTransferencia;
+
+	// Getters e Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContaOrigem() {
+		return contaOrigem;
+	}
+
+	public void setContaOrigem(String contaOrigem) {
+		this.contaOrigem = contaOrigem;
+	}
+
+	public String getContaDestino() {
+		return contaDestino;
+	}
+
+	public void setContaDestino(String contaDestino) {
+		this.contaDestino = contaDestino;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public BigDecimal getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(BigDecimal taxa) {
+		this.taxa = taxa;
+	}
+
+	public LocalDate getDataAgendamento() {
+		return dataAgendamento;
+	}
+
+	public void setDataAgendamento(LocalDate dataAgendamento) {
+		this.dataAgendamento = dataAgendamento;
+	}
+
+	public LocalDate getDataTransferencia() {
+		return dataTransferencia;
+	}
+
+	public void setDataTransferencia(LocalDate dataTransferencia) {
+		this.dataTransferencia = dataTransferencia;
+	}
+}
